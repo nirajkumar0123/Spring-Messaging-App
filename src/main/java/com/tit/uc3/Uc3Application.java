b8655465c5daf0d976ed2859a87548beb2f54813
Niraj Kumar <nirajgupta7481@gmail.com>
2025-02-21 19:23:25 +0530

    [Niraj] Add . uc3 feature added

---
+++ src/main/java/com/tit/uc3/Uc3Application.java
@@ -0,0 +1,13 @@
+package com.tit.uc3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc3Application.class, args);
+	}
+
+}
