4e887b71b0a05bd6bd8f388de9127b20dcd99e30
Niraj Kumar <nirajgupta7481@gmail.com>
2025-02-21 19:03:09 +0530

    [Niraj] Add . uc2 feature added

---
+++ src/main/java/com/tit/uc2/Uc2Application.java
@@ -0,0 +1,13 @@
+package com.tit.uc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc2Application.class, args);
+	}
+
+}
